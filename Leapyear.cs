c9f2afbf5d0e6b31111bd4f1981c4947d709b785
Geetanjali Koli <k0ligeet@hotmail.com>
2022-05-21 08:47:12 +0530

    [Geetanjali_Koli] Practise problem of Leapyear

---
+++ Leapyear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProblems
+{
+    internal class Leapyear
+    {
+        public static void Year(string[] args)
+        {
+            Console.WriteLine("Enter the year");
+            int yearvalue = Convert.ToInt32(Console.ReadLine());
+
+            if (yearvalue % 4 == 0)
+            {
+                Console.WriteLine("The year " + yearvalue + "is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("The year " + yearvalue + " is not a leap year");
+            }
+
+        }
+    }
+}
